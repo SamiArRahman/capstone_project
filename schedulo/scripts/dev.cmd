37384a9967dc6d693b9fb78d4c81b47a8d950238
SamiArRahman <samivsshanita@gmail.com>
2026-04-04 22:49:13 -0400

    Revert "fixing errors"
    This reverts commit 868de8fea065bee4450bc19b5d3eec7ca0695841.

--- schedulo/scripts/dev.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-set ROOT=%~dp0..
-
-start "Schedulo Server" cmd /k "cd /d \"%ROOT%\" && npm --workspace server run dev"
-start "Schedulo Client" cmd /k "cd /d \"%ROOT%\" && npm --workspace client start"
-
-echo Started Schedulo server and client in separate windows.
