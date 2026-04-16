845590b65b4c245fc61cf0e801c361f5c0ee6073
SamiArRahman <samivsshanita@gmail.com>
2026-04-16 17:30:02 -0400

    vercel routing fix

--- schedulo/api/index.js
+++ schedulo/api/index.js
@@ -1,1 +1,22 @@
-module.exports = require("../server/src/app");
+const app = require("../server/src/app");
+
+module.exports = function vercelHandler(req, res) {
+  const requestUrl = new URL(req.url, "http://localhost");
+  const forwardedRoute = requestUrl.searchParams.get("__route");
+
+  if (forwardedRoute != null) {
+    requestUrl.searchParams.delete("__route");
+
+    const normalizedRoute = String(forwardedRoute || "")
+      .split("/")
+      .filter(Boolean)
+      .join("/");
+
+    const nextPath = normalizedRoute ? `/api/${normalizedRoute}` : "/api";
+    const nextQuery = requestUrl.searchParams.toString();
+
+    req.url = nextQuery ? `${nextPath}?${nextQuery}` : nextPath;
+  }
+
+  return app(req, res);
+};
